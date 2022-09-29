cf8a6b01e8fce5039e48a3834f1f3ea0073a8c10
Zygiel <zygiel93@gmail.com>
2022-09-29 10:22:45 +0200

    Add CategoryWithTickets Query and handler

---
+++ Zygieldesk.Application/Functions/Categories/Queries/GetCategoryWithTickets/CategoryWithTitcketsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zygieldesk.Application.Functions.Categories.Queries.GetCategoryWithTickets
+{
+    public class CategoryWithTitcketsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<CategoryTicketDto> Tickets { get; set; }
+    }
+}
---
+++ Zygieldesk.Application/Functions/Categories/Queries/GetCategoryWithTickets/GetCategoryWithTicketsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zygieldesk.Application.Contracts.Persistance;
+
+namespace Zygieldesk.Application.Functions.Categories.Queries.GetCategoryWithTickets
+{
+    public class GetCategoryWithTicketsQueryHandler : IRequestHandler<GetCategoryWithTicketsQuery, CategoryWithTitcketsViewModel>
+    {
+        private readonly IMapper _mapper;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public GetCategoryWithTicketsQueryHandler(IMapper mapper, ICategoryRepository categoryRepository)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<CategoryWithTitcketsViewModel> Handle(GetCategoryWithTicketsQuery request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.GetByIdAsync(request.CategoryId);
+
+            return _mapper.Map<CategoryWithTitcketsViewModel>(category);
+        }
+    }
+}
