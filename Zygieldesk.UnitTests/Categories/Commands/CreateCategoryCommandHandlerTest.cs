20047f8a0756e605e9e3612588c126adb6ad4003
Zygiel <zygiel93@gmail.com>
2022-09-29 14:02:05 +0200

    Add UnitTest for CreateCategoryCommandHandler

---
+++ Zygieldesk.UnitTests/Categories/Commands/CreateCategoryCommandHandlerTest.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zygieldesk.Application.Contracts.Persistance;
+using Zygieldesk.Application.Functions.Categories.Commands.CreateCategory;
+using Zygieldesk.Application.Mapper;
+using Zygieldesk.UnitTests.Mocks;
+
+namespace Zygieldesk.UnitTests.Categories.Commands
+{
+    public class CreateCategoryCommandHandlerTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ICategoryRepository> _mockCategoryRepository;
+
+        public CreateCategoryCommandHandlerTest()
+        {
+            _mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Handle_ValidCategory_AddedToCategoriesRepo()
+        {
+            var handler = new CreatedCategoryCommandHandler(_mapper, _mockCategoryRepository.Object);
+            var allCategoriesBeforeCount = (await _mockCategoryRepository.Object.GetAllAsync()).Count;
+
+            var response = await handler.Handle(new CreatedCategoryCommand()
+            {
+                Name = "Test",
+                Description = "Test"
+            }, CancellationToken.None);
+
+            var allCategories = await _mockCategoryRepository.Object.GetAllAsync();
+
+            response.Success.ShouldBe(true);
+            response.ValidationErrors.Count.ShouldBe(0);
+            allCategories.Count.ShouldBe(allCategoriesBeforeCount + 1);
+            response.CategoryId.ShouldNotBeNull();
+
+            var response2 = await handler.Handle(new CreatedCategoryCommand()
+            {
+                Name = "Test2",
+                Description = "Test2"
+            }, CancellationToken.None);
+
+            response2.Success.ShouldBe(true);
+            response2.ValidationErrors.Count.ShouldBe(0);
+            allCategories.Count.ShouldBe(allCategoriesBeforeCount + 2);
+            response2.CategoryId.ShouldNotBeNull();
+        }
+
+    }
+}
