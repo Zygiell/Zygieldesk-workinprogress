889574fc27fb4b5c1921dc9bd0eb3473ea0cc608
Zygiel <zygiel93@gmail.com>
2022-09-29 13:44:16 +0200

    Add BaseResponse, ResponseStatus, Create Category Command logic

---
+++ Zygieldesk.Application/Functions/Categories/Commands/CreateCategory/CreatedCategoryCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zygieldesk.Application.Contracts.Persistance;
+using Zygieldesk.Domain.Entities;
+
+namespace Zygieldesk.Application.Functions.Categories.Commands.CreateCategory
+{
+    public class CreatedCategoryCommandHandler : IRequestHandler<CreatedCategoryCommand, CreatedCategoryCommandResponse>
+    {
+        private readonly IMapper _mapper;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CreatedCategoryCommandHandler(IMapper mapper, ICategoryRepository categoryRepository)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<CreatedCategoryCommandResponse> Handle(CreatedCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreatedCategoryCommandValidator();
+            var validatorResult = await validator.ValidateAsync(request);
+
+            if (!validatorResult.IsValid)
+            {
+                return new CreatedCategoryCommandResponse(validatorResult);
+            }
+
+            var category = _mapper.Map<Category>(request);
+
+            category = await _categoryRepository.AddAsync(category);
+
+            return new CreatedCategoryCommandResponse(category.Id);
+        }
+    }
+}
---
+++ Zygieldesk.Application/Functions/Responses/ResponseStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zygieldesk.Application.Functions.Responses
+{
+    public enum ResponseStatus
+    {
+        Succes = 0,
+        NotFound = 1,
+        BadQueryRequest = 2,
+        ValidationError = 3,
+        Exception = 4,
+        DataBaseError = 5,
+        OtherClientApiError = 6
+    }
+}
