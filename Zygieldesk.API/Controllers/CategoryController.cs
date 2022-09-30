e5307019309fba80ae1e3c1e410cd2f65299a2ba
Zygiel <zygiel93@gmail.com>
2022-09-30 14:10:34 +0200

    Add Category Controller, modify Program.cs

---
+++ Zygieldesk.API/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zygieldesk.Application.Functions.Categories.Queries.GetCategoryList;
+
+namespace Zygieldesk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("all", Name ="GetAllCategories")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryListViewModel>>> GetAllCategories()
+        {
+            var categoryListViewModel = await _mediator.Send(new GetCategoryListQuery());
+            return Ok(categoryListViewModel);
+        }
+    }
+}
--- Zygieldesk.API/Program.cs
+++ Zygieldesk.API/Program.cs
@@ -1,20 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Zygieldesk.Application;
+using Zygieldesk.Persistance;
+using Zygieldesk.Persistance.Seeder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddZygieldeskApplication();
+builder.Services.AddZygieldeskPersistanceEFServices(builder.Configuration);
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Open",
+        builder => builder.AllowAnyOrigin()
+        .AllowAnyHeader().AllowAnyMethod());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ZygieldeskDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ZygieldeskConnectionString")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<ZygieldeskSeeder>();
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("Open");
+
+seeder.Seed();
 
 app.UseHttpsRedirection();
 
