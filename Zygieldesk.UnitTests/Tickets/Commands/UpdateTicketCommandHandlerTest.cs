08f9eafb786ca8a680bde200a354c10b7363ff55
Zygiel <zygiel93@gmail.com>
2022-10-03 22:12:59 +0200

    Add CRUD Unit Tests for Ticket

---
+++ Zygieldesk.UnitTests/Tickets/Commands/UpdateTicketCommandHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zygieldesk.Application.Contracts.Persistance;
+using Zygieldesk.Application.Functions.Tickets.Commands.UpdateTicket;
+using Zygieldesk.Application.Mapper;
+using Zygieldesk.UnitTests.Mocks;
+
+namespace Zygieldesk.UnitTests.Tickets.Commands
+{
+    public class UpdateTicketCommandHandlerTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ITicketRepository> _mockTicketRepository;
+        public UpdateTicketCommandHandlerTest()
+        {
+            _mockTicketRepository = RepositoryMocks.GetTicketRepository();
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            _mapper = configuration.CreateMapper();
+        }
+
+        [Fact]
+        public async Task UpdateTicketTest()
+        {
+            var handler = new UpdateTicketCommandHandler(_mapper, _mockTicketRepository.Object);
+            var result = await handler.Handle(new UpdateTicketCommand { TicketBody = "New body", TicketTitle = "New name", TicketId = 1 }, CancellationToken.None);
+            var ticketToUpdate = await _mockTicketRepository.Object.GetByIdAsync(1);
+
+            result.ShouldBeOfType<UpdateTicketCommandResponse>();
+            ticketToUpdate.TicketTitle.ShouldBe("New name");
+
+        }
+    }
+}
---
+++ Zygieldesk.UnitTests/Tickets/Queries/GetAllTicketsQueryTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zygieldesk.Application.Contracts.Persistance;
+using Zygieldesk.Application.Functions.Tickets.Queries.GetAllTickets;
+using Zygieldesk.Application.Functions.Tickets.Queries.GetTicketById;
+using Zygieldesk.Application.Mapper;
+using Zygieldesk.UnitTests.Mocks;
+
+namespace Zygieldesk.UnitTests.Tickets.Queries
+{
+    public class GetAllTicketsQueryTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ITicketRepository> _mockTicketRepository;
+        public GetAllTicketsQueryTests()
+        {
+            _mockTicketRepository = RepositoryMocks.GetTicketRepository();
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            _mapper = configuration.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetAllTicketsTests()
+        {
+            var handler = new GetAllTicketsQueryHandler(_mapper, _mockTicketRepository.Object);
+            var result = await handler.Handle(new GetAllTicketsQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<TicketViewModel>>();
+            result.Count.ShouldBe(9);
+        }
+    }
+}
